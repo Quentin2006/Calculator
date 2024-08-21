728ba94c5dc04c5318ebebc7f2155e9b239758c1
Quentin Scheetz <124484154+Quentin2006@users.noreply.github.com>
2024-08-21 14:54:56 -0700

    logic 100% done

--- scripts.js
+++ scripts.js
@@ -34,18 +34,18 @@ let num2 = "";
 function populateDisplay(value) {
   const display = document.querySelector(".display");
   let operations = ["addition", "subtraction", "multiplacation", "division"];
-  let nums = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  let nums = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "decimal"];
 
   if (nums.includes(value)) {
     if (num1 === "" || !operationSelected) {
-      num1 += value;
-      display.textContent = num1
+      if (value === "decimal") num1 += ".";
+      else num1 += value;
+      display.textContent = num1;
+    } else {
+      if (value === "decimal") num2 += ".";
+      else num2 += value;
+      display.textContent = num2;
     }
-    else {
-      num2 += value;
-      display.textContent = num2
-    }
-
   } else if (operations.includes(value)) {
     operationSelected = true;
     if (num1 != "" && num2 != "") {
@@ -55,54 +55,42 @@ function populateDisplay(value) {
       num2 = "";
 
       display.textContent = num1;
-    }     
+    }
     operation = value;
-
-  } else if (value === "equals") {
-
+  } else if (value === "equals" && num1 != "" && num2 != "") {
     let product = operate(+num1, operation, +num2);
 
     num1 = product;
     num2 = "";
 
-    display.textContent = num1
+    if (num1 >= 100000000 || num1 <= 9.999999999)
+      display.textContent = num1.toExponential(6);
+    else display.textContent = num1;
   } else if (value === "clear") {
-    operationSelected = false
-    operation = undefined
-    num1 = ""
-    num2 = ""
-    display.textContent = ""
+    operationSelected = false;
+    operation = undefined;
+    num1 = "";
+    num2 = "";
+    display.textContent = "";
   } else if (value === "delete") {
     if (num2 === "") {
-      num1 = num1.split("")
-      num1.pop()
-      num1 = num1.join("")
-      display.textContent = num1
-
-    } else  {
-      num2 = num2.split("")
-      num2.pop()
-      num2 = num2.join("")
-      display.textContent = num2
+      num1 = num1.split("");
+      num1.pop();
+      num1 = num1.join("");
+      display.textContent = num1;
+    } else {
+      num2 = num2.split("");
+      num2.pop();
+      num2 = num2.join("");
+      display.textContent = num2;
     }
-
   } else if (value === "plus-minus") {
     if (num2 === "") {
-      num1 = num1 * -1
-      display.textContent = num1
-
-    } else  {
-      num2 = num2 * -1
-      display.textContent = num2
-    }
-  } else if (value === "decimal") {
-    if (num2 === "") {
-      num1 += '.'
-      display.textContent = num1
-
-    } else  {
-      num2 += '.'
-      display.textContent = num2
+      num1 = num1 * -1;
+      display.textContent = num1;
+    } else {
+      num2 = num2 * -1;
+      display.textContent = num2;
     }
   }
 }
