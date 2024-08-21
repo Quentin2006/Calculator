fce29226fabb6db1d5df8a6ac7a77f55499dc97d
Quentin Scheetz <124484154+Quentin2006@users.noreply.github.com>
2024-08-20 18:39:39 -0700

    most of the logic is done. just struggling with final details

--- scripts.js
+++ scripts.js
@@ -14,21 +14,60 @@ function divide(num1, num2) {
   return num1 / num2;
 }
 
-function operate() {
-  let num1 = prompt("Enter your number");
-  let operation = prompt("Enter your operation");
-  let num2 = prompt("Enter your number");
-
+function operate(num1, operation, num2) {
   switch (true) {
-    case operation == "+":
+    case operation == "addition":
       return add(num1, num2);
-    case operation == "-":
+    case operation == "subtraction":
       return subtract(num1, num2);
-    case operation == "*":
+    case operation == "multiplacation":
       return multiply(num1, num2);
-    case operation == "/":
+    case operation == "division":
       return divide(num1, num2);
   }
 }
 
-console.log(operate());
+let nums = [];
+let operation;
+
+function populateDisplay(value) {
+  const display = document.querySelector(".display");
+  let operations = ["addition", "subtraction", "multiplacation", "division"];
+
+  if (typeof value === "number") {
+    // adds num to nums array
+    nums.push(value);
+    // adds num to screen
+    console.log(display.textContent.split("").length);
+    if (operation != undefined && display.textContent.split("").length >= 1) {
+      display.textContent = value;
+    } else display.textContent += value;
+  } else if (value === "clear") {
+    // clears display and resets nums array
+    display.textContent = "";
+    nums.length = 0;
+  } else if (value === "delete") {
+    // makes display value into an array, removes last value,
+    // and combines array into string
+    let tempDisplay = display.textContent;
+    tempDisplay = tempDisplay.split("");
+    tempDisplay.pop();
+    display.textContent = tempDisplay.join("");
+  } else if (value === "plus-minus") {
+    display.textContent = display.textContent * -1;
+  } else if (value === "decimal") {
+    display.textContent += ".";
+  } else if (operations.includes(value)) {
+    operation = value;
+    if (nums.length >= 2) {
+      nums[0] = operate(+nums[0], operation, +nums[1]);
+      nums.length = 1;
+      display.textContent = nums[0];
+    }
+    console.log(value, nums);
+  } else if (value === "equals") {
+    nums[0] = operate(+nums[0], operation, +nums[1]);
+    nums.length = 1;
+    display.textContent = nums[0];
+  }
+}
